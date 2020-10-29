4207d9cd659b8f3f04d7108fc0c223f54f0a5550
oti95 <onamesnic@yahoo.com>
2020-10-29 15:18:02 +0200

    tema pe acasa

--- MarathonLesson2/MarathonLesson2/Program.cs
+++ MarathonLesson2/MarathonLesson2/Program.cs
@@ -9,25 +9,17 @@ namespace MarathonLesson2
     {
         static void Main(string[] args)
         {
-            decimal elev1, elev2, elev3, elev4, elev5, media, scorul;
+            decimal Celsius, Kelvin, Fahrenheit;
 
-            Console.WriteLine("Introduceti punctajul: ");
-            Console.Write("Elevul 1: ");
-            elev1 = Convert.ToDecimal(Console.ReadLine());
-            Console.Write("Elevul 2: ");
-            elev2 = Convert.ToDecimal(Console.ReadLine());
-            Console.Write("Elevul 3: ");
-            elev3 = Convert.ToDecimal(Console.ReadLine());
-            Console.Write("Elevul 4: ");
-            elev4 = Convert.ToDecimal(Console.ReadLine());
-            Console.Write("Elevul 5: ");
-            elev5 = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Introdu temperatura: ");
+            Celsius = Convert.ToDecimal(Console.ReadLine());
 
-            media = (elev1 + elev2 + elev3 + elev4 + elev5) / 5m;
-            scorul = elev1 + elev2 + elev3 + elev4 + elev5;
+            Kelvin = Celsius + 275.15m;
 
-            Console.WriteLine("Media este: " + media);
-            Console.WriteLine("Scorul total este: " + scorul);
+            Fahrenheit = Celsius * 1.8m + 32m;
+
+            Console.WriteLine("Kelvin: " + Kelvin);
+            Console.WriteLine("Fahrenheit:" + Fahrenheit);
 
 
 
